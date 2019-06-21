1d84f72d80ba9121cab4ed2dc4fd67b0cc1de92e
Hanno <h.baensch.92@gmail.com>
2019-06-21 12:28:32 +0200

    Small fix in demo shader

--- demos/uv-warp.glsl
+++ demos/uv-warp.glsl
@@ -1,6 +1,5 @@
 void main() {
     vec2 uv = gl_FragCoord.xy / iResolution.xy;
-    float time = iGlobalTime;
  
     for (float i = 1.0; i < 10.0; i++) {
         float v = 0.1 * sin((uv.r + uv.g) * 0.5 * i + iTime) / i;
