6a4b405899687759f1e5dfdae5069e31244f2ea2
Hanno <h.baensch.92@gmail.com>
2019-06-21 13:13:51 +0200

    Added duck demo shader

---
+++ demos/duck.glsl
@@ -0,0 +1,10 @@
+#iChannel0 "https://cdn-images-1.medium.com/max/1200/1*eFjpoz8lhNKyd8XcSWv2OA.jpeg"
+
+void main() {
+    vec2 uv = (gl_FragCoord.xy / iResolution.xy);
+    uv.y += 0.05 * sin(iTime + uv.x * 10.0);
+    uv.x += 0.05 * sin(iTime + uv.y * 10.0);
+    vec4 color = texture(iChannel0, uv);
+    color = color * color;
+    gl_FragColor = color;
+}
