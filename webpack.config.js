776ef409a1dd6f24772db6cd740cb9ba44ff2f4b
Jackson Kearl <jakearl@microsoft.com>
2021-10-14 02:44:18 +0000

    Try web config

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,59 @@
 'use strict';
 
 const path = require('path');
+const webpack = require("webpack");
 
-/**@type {import('webpack').Configuration}*/
-const config = {
+const webConfig = /** @type WebpackConfig */ {
+  context: __dirname,
+  mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  target: "webworker", // web extensions run in a webworker context
+  entry: {
+    "extension-web": "./src/extension.ts", // source of the web extension main file
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "./dist"),
+    libraryTarget: "commonjs",
+  },
+  node: {
+    fs: 'empty'
+  },
+  resolve: {
+    mainFields: ["browser", "module", "main"], // look for `browser` entry point in imported node modules
+    extensions: [".ts", ".js"], // support ts-files and js-files
+    alias: {
+      // provides alternate implementation for node module and source files
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: "process/browser", // provide a shim for the global `process` variable
+    }),
+  ],
+  externals: {
+    vscode: "commonjs vscode", // ignored because it doesn't exist
+  },
+  performance: {
+    hints: false,
+  },
+  devtool: "nosources-source-map", // create a source map that points to the original source file
+};
+
+
+const nodeConfig = /** @type WebpackConfig */ {
   target: 'node',
   entry: './src/extension.ts',
   output: {
@@ -35,4 +85,4 @@ const config = {
     ]
   }
 };
-module.exports = config;
+module.exports = [nodeConfig, webConfig];
