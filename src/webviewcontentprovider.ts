277c261651bd71ffd6eb07f31f841f1d4fad9700
shadielhajj <shadi.elhajj@gmail.com>
2024-05-21 17:40:07 +0100

    Added the shader-toy.recordOfflineQuality setting.

--- src/webviewcontentprovider.ts
+++ src/webviewcontentprovider.ts
@@ -73,6 +73,7 @@ import { RecordVideoContainerExtension } from './extensions/user_interface/recor
 import { RecordVideoCodecExtension } from './extensions/user_interface/record_video_codec_extension';
 import { RecordVideoBitRateExtension } from './extensions/user_interface/record_video_bit_rate_extension';
 import { RecordMaxDurationExtension } from './extensions/user_interface/record_max_duration_extension';
+import { RecordOfflineQualityExtension } from './extensions/user_interface/record_offline_quality_extension';
 
 export class WebviewContentProvider {
     private context: Context;
@@ -420,6 +421,10 @@ export class WebviewContentProvider {
         const recordMaxDuration = this.context.getConfig<number>('recordMaxDuration') || 0;
         const recordMaxDurationExtension = new RecordMaxDurationExtension(recordMaxDuration);
         this.webviewAssembler.addReplaceModule(recordMaxDurationExtension, 'let maxDuration = <!-- Record Max Duration -->;', '<!-- Record Max Duration -->');
+
+        const recordOfflineQuality = this.context.getConfig<number>('recordOfflineQuality') || 80;
+        const recordOfflineQualityExtension = new RecordOfflineQualityExtension(recordOfflineQuality);
+        this.webviewAssembler.addReplaceModule(recordOfflineQualityExtension, 'let quality = <!-- Record Offline Quality -->;', '<!-- Record Offline Quality -->');
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // Reload Logic
