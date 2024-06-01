2da008a61a3c0e6b61ea360af5c57e6a5dac368f
Hanno <vingine.dev@gmail.com>
2024-06-01 12:52:57 +0200

    Update some packages to get tests runnning again

--- test/run_tests.ts
+++ test/run_tests.ts
@@ -3,14 +3,14 @@ import * as path from 'path';
 import { runTests } from '@vscode/test-electron';
 
 async function main() {
-	try {
-		const extensionDevelopmentPath = path.resolve(__dirname, '../');
-		const extensionTestsPath = path.resolve(__dirname, './index');
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
-	} catch (err) {
-		console.error('Failed to run tests');
-		process.exit(1);
-	}
+    try {
+        const extensionDevelopmentPath = path.resolve(__dirname, '../');
+        const extensionTestsPath = path.resolve(__dirname, './index');
+        await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    } catch (err) {
+        console.error('Failed to run tests');
+        process.exit(1);
+    }
 }
 
 main();
