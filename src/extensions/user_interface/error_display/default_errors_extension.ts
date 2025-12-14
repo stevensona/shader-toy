f5e829de95ef4aaa340ca3956ac63a10129adacd
rtj <r.t.janik@gmail.com>
2025-12-14 21:05:08 +0100

    Fixed error code lines diagnostics with multi-level includes support

--- src/extensions/user_interface/error_display/default_errors_extension.ts
+++ src/extensions/user_interface/error_display/default_errors_extension.ts
@@ -5,19 +5,46 @@ import { WebviewExtension } from '../../webview_extension';
 export class DefaultErrorsExtension implements WebviewExtension {
     public generateContent(): string {
         return `\
-console.error = function (message) {
-    if('7' in arguments) {
-        let message = arguments[7].replace(/ERROR: \\d+:(\\d+):\\W(.*)\\n/g, function(match, line, error) {
-            let lineNumber = Number(line) - currentShader.LineOffset;
-            let lineHighlight = \`${'<a class=\'error\' unselectable onclick=\'revealError(${lineNumber}, "${currentShader.File}")\'>Line ${lineNumber}</a>'}\`;
-            return \`<li>\${lineHighlight}: \${error}</li>\`;
-        });
+(() => {
+    const consoleError = console.error ? console.error.bind(console) : undefined;
+    console.error = function () {
+        try {
+            // The shader info log can appear in different argument positions.
+            let rawErrors = undefined;
+            for (let i = arguments.length - 1; i >= 0; i--) {
+                const v = arguments[i];
+                if (typeof v === 'string' && v.indexOf('ERROR:') >= 0) {
+                    rawErrors = v;
+                    break;
+                }
+            }
 
-        $('#message').append(\`<h3>Shader failed to compile - \${currentShader.Name} </h3>\`);
-        $('#message').append('<ul>');
-        $('#message').append(message);
-        $('#message').append('</ul>');
-    }
-};`;
+            if (rawErrors !== undefined && currentShader && currentShader.LineOffset !== undefined) {
+                // Typical WebGL log format:
+                //   ERROR: <sourceId>:<line>: <message>
+                let message = rawErrors.replace(/ERROR:\\s*(\\d+):(\\d+):\\W(.*?)(?:\\n|$)/g, function(match, sourceId, line, error) {
+                    const sid = Number(sourceId);
+                    let lineNumber = Number(line) - (currentShader.LineOffset || 0);
+                    const file = (sid === 0)
+                        ? currentShader.File
+                        : ((typeof sourceIdToFile === 'object' && sourceIdToFile[sid]) ? sourceIdToFile[sid] : currentShader.File);
+                    let lineHighlight = "<a class='error' unselectable onclick='revealError(" + lineNumber + ", " + JSON.stringify(file) + ")'>Line " + lineNumber + "</a>";
+                    return '<li>' + lineHighlight + ': ' + error + ' <span>(' + file + ')</span></li>';
+                });
+
+                $('#message').append(\`<h3>Shader failed to compile - \${currentShader.Name} </h3>\`);
+                $('#message').append('<ul>');
+                $('#message').append(message);
+                $('#message').append('</ul>');
+            }
+        } catch (e) {
+            // Ignore and fall through.
+        }
+
+        if (consoleError) {
+            consoleError.apply(console, arguments);
+        }
+    };
+})();`;
     }
 }
--- src/extensions/user_interface/error_display/diagnostics_errors_extension.ts
+++ src/extensions/user_interface/error_display/diagnostics_errors_extension.ts
@@ -5,42 +5,70 @@ import { WebviewExtension } from '../../webview_extension';
 export class DiagnosticsErrorsExtension implements WebviewExtension {
     public generateContent(): string {
         return `\
-console.error = function () {
-    if('7' in arguments) {
-        let errorRegex = /ERROR: \\d+:(\\d+):\\W(.*)\\n/g;
-        let rawErrors = arguments[7];
-        let match;
-        
-        let diagnostics = [];
-        let message = '';
-        while(match = errorRegex.exec(rawErrors)) {
-            let lineNumber = Number(match[1]) - currentShader.LineOffset;
-            let error = match[2];
-            diagnostics.push({
-                line: lineNumber,
-                message: error
-            });
-            let lineHighlight = \`${'<a class=\'error\' unselectable onclick=\'revealError(${lineNumber}, "${currentShader.File}")\'>Line ${lineNumber}</a>'}\`;
-            message += \`<li>\${lineHighlight}: \${error}</li>\`;
-        }
-        console.log(message);
-        let diagnosticBatch = {
-            filename: currentShader.File,
-            diagnostics: diagnostics
-        };
-        if (vscode !== undefined) {
-            vscode.postMessage({
-                command: 'showGlslDiagnostic',
-                type: 'error',
-                diagnosticBatch: diagnosticBatch
-            });
+(() => {
+    const consoleError = console.error ? console.error.bind(console) : undefined;
+    console.error = function () {
+        try {
+            let rawErrors = undefined;
+            for (let i = arguments.length - 1; i >= 0; i--) {
+                const v = arguments[i];
+                if (typeof v === 'string' && v.indexOf('ERROR:') >= 0) {
+                    rawErrors = v;
+                    break;
+                }
+            }
+
+            if (rawErrors !== undefined && currentShader && currentShader.LineOffset !== undefined) {
+                let errorRegex = /ERROR:\\s*(\\d+):(\\d+):\\W(.*?)(?:\\n|$)/g;
+                let match;
+
+                let message = '';
+                const diagnosticsByFile = {};
+
+                while (match = errorRegex.exec(rawErrors)) {
+                    const sid = Number(match[1]);
+                    const rawLine = Number(match[2]);
+                    const error = match[3];
+                    const file = (sid === 0)
+                        ? currentShader.File
+                        : ((typeof sourceIdToFile === 'object' && sourceIdToFile[sid]) ? sourceIdToFile[sid] : currentShader.File);
+                    const lineNumber = rawLine - (currentShader.LineOffset || 0);
+
+                    if (diagnosticsByFile[file] === undefined) {
+                        diagnosticsByFile[file] = [];
+                    }
+                    diagnosticsByFile[file].push({ line: lineNumber, message: error });
+
+                    let lineHighlight = "<a class='error' unselectable onclick='revealError(" + lineNumber + ", " + JSON.stringify(file) + ")'>Line " + lineNumber + "</a>";
+                    message += '<li>' + lineHighlight + ': ' + error + ' <span>(' + file + ')</span></li>';
+                }
+
+                if (vscode !== undefined) {
+                    for (const filename in diagnosticsByFile) {
+                        vscode.postMessage({
+                            command: 'showGlslDiagnostic',
+                            type: 'error',
+                            diagnosticBatch: {
+                                filename: filename,
+                                diagnostics: diagnosticsByFile[filename]
+                            }
+                        });
+                    }
+                }
+
+                $('#message').append(\`<h3>Shader failed to compile - \${currentShader.Name} </h3>\`);
+                $('#message').append('<ul>');
+                $('#message').append(message);
+                $('#message').append('</ul>');
+            }
+        } catch (e) {
+            // Ignore and fall through.
         }
 
-        $('#message').append(\`<h3>Shader failed to compile - \${currentShader.Name} </h3>\`);
-        $('#message').append('<ul>');
-        $('#message').append(message);
-        $('#message').append('</ul>');
-    }
-};`;
+        if (consoleError) {
+            consoleError.apply(console, arguments);
+        }
+    };
+})();`;
     }
 }
